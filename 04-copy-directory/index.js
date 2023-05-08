97520911a27b5cc276ebe153fee75c884b980cd1
githorder <diyor.baynazarov2000@gmail.com>
2023-05-09 02:57:35 +0500

    feat: copy directory task complete (Tue, Mon 9 02:57 AM)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const path = require("node:path");
+const fsPromises = require("node:fs/promises");
 
+const copyFilesFrom = async (dir) => {
+  try {
+    const files = await fsPromises.readdir(dir);
+    await fsPromises.mkdir(path.join(__dirname, "files-copy"), {
+      recursive: true,
+    });
+
+    files.forEach(async (file) => {
+      const stat = await fsPromises.stat(path.join(dir, file));
+
+      if (stat.isFile()) {
+        await fsPromises.copyFile(
+          path.join(dir, file),
+          path.join(__dirname, "files-copy", file)
+        );
+      }
+    });
+  } catch (err) {
+    console.log(`Unable to copy files from ${dir} to files-copy`);
+  }
+};
+
+copyFilesFrom(path.join(__dirname, "./files"));
